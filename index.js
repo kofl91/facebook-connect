44de830abeeae2670456fadf3728d530c8b6da84
kofl91 <kofl91@github.com>
2016-09-29 16:54:13 +0200

    [NOT WORKING]

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
+var myUserId;
+
 // This function is called when a Users status was changed to logged in.
 // Hides the login-button
 // Displays the logout-button
 function changeToLoggedIn(){
   document.getElementById('fb-loginbutton').style.display = "none";
-  document.getElementById('fb-logoutbutton').style.display = "";
+  document.getElementById('fb-logoutbutton').style.display = "block";
+  document.getElementById('details').style.display = "block";
 }
 
 // This function is called when a Users status was changed to logged out.
 // Displays the login-button
 // Hides the logout-button
 function changeToLoggedOut(){
-  document.getElementById('fb-loginbutton').style.display = "";
+  document.getElementById('fb-loginbutton').style.display = "block";
   document.getElementById('fb-logoutbutton').style.display = "none";
+  document.getElementById('details').style.display = "none";
 }
 
+
+// Displays the authResponse.
+function displayAuthResponse(authResponse){
+
+  var myNode = document.getElementById('accessTokenList');
+  while (myNode.firstChild) {
+      myNode.removeChild(myNode.firstChild);
+  }
+
+  var listEntry = document.createElement('li');
+  listEntry.innerHTML = "AccessToken : "+ authResponse.accessToken;
+  document.getElementById('accessTokenList').appendChild(listEntry);
+
+  listEntry = document.createElement('li');
+  listEntry.innerHTML = "Expires in : " + authResponse.expiresIn +" seconds";
+  document.getElementById('accessTokenList').appendChild(listEntry);
+
+  listEntry = document.createElement('li');
+  listEntry.innerHTML = "Signed Request : " + authResponse.signedRequest;
+  document.getElementById('accessTokenList').appendChild(listEntry);
+
+  listEntry = document.createElement('li');
+  listEntry.innerHTML = "UserId : " + authResponse.userID;
+  document.getElementById('accessTokenList').appendChild(listEntry);
+}
 
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
@@ -21,6 +50,8 @@ function statusChangeCallback(response) {
   // app know the current login status of the person.
   // Full docs on the response object can be found in the documentation
   // for FB.getLoginStatus().
+  console.log(response);
+
   if (response.status === 'connected') {
     // Logged into your app and Facebook.
     testAPI();
@@ -36,6 +67,9 @@ function statusChangeCallback(response) {
     document.getElementById('status').innerHTML = 'Please log ' +
       'into Facebook.';
     changeToLoggedOut();
+  }
+  if (response.authResponse){
+    displayAuthResponse(response.authResponse);
   }
 }
 
@@ -61,7 +95,7 @@ window.fbAsyncInit = function() {
     appId      : '393238210800116',
     cookie     : true,  // enable cookies to allow the server to access
                         // the session
-    xfbml      : true,  // parse social plugins on this page
+    xfbml      : false,  // parse social plugins on this page
     version    : 'v2.5' // use graph api version 2.5
   });
 
@@ -85,6 +119,7 @@ function testAPI() {
   FB.api('/me', function(response) {
     document.getElementById('status').innerHTML =
       'Thanks for logging in, ' + response.name + '!';
+    myUserId = response.id;
   });
 }
 
@@ -94,3 +129,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     'click',
     logoutUser);
 });
+
+function getParameter(parameter)
+{
+  FB.api(
+      "/"+parameter,
+      function (response) {
+        if (response && !response.error) {
+          /* handle the result */
+          console.log(response);
+        }
+      }
+  );
+}
